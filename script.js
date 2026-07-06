7703df25543187e9902b11d33c3a9ac1dabfab37
Satish Kumar Villa <villasatishkumar17@gmail.com>
2026-07-06 11:16:40 +0530

    Initial portfolio

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll("a").forEach(anchor => {
+
+    anchor.addEventListener("click", function (e) {
+
+        const href = this.getAttribute("href");
+
+        if (href.startsWith("#")) {
+            e.preventDefault();
+
+            document.querySelector(href).scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+
+    });
+
+});
